c8463267608de22d4ddafb328a786d3fa74081fe
Guilherme Siqueira <gnerd0104@gmail.com>
2022-06-07 22:29:38 -0300

    commitzin

---
+++ api/src/repository/vilaorepository.js
@@ -0,0 +1,27 @@
+import { con } from './connection.js';
+
+export async function inserirvilao(vilao) {
+    const comando = 
+    `
+    INSERT INTO tb_vilao (nm_vilao, ds_maldades, bt_super_poder)
+                  VALUES (?, ?, ?) `
+    
+    const [resposta] = await con.query(comando,[vilao.vilao, vilao.maldades, vilao.superpoder]); 
+  vilao.id =  resposta.insertId;
+  
+  return vilao;
+}
+
+export async function listadeviloes() {
+  const comando = 
+  `
+  SELECT id_vilao			      id,
+	       nm_vilao			      nome,
+         ds_maldades		    maldades,
+         bt_super_poder	    superpoder,
+  FROM   tb_vilao `
+  
+  const [linhas] = await con.query(comando);
+  return linhas;
+  
+}
